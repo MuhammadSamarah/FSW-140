cddf18aa266152582f96ec300142c9b1ac533e2f
MuhammadSamarah <muhammad.samarah@bryanuniversity.edu>
2021-10-31 12:13:07 -0500

    update

---
+++ server.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/moviesdb',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  },
+  () => console.log("Connected to the DB")
+)
